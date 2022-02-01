8090d42eb8be6155aa545cb3e1b4519888244b68
Felipe Pascucci <feliperpascucci@gmail.com>
2022-02-01 17:33:31 -0300

    Atualizações no banco

---
+++ banco/banco/cliente/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from cliente import models
+
+class ClienteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Clientes
+        fields = '__all__'
+
+class DepositoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Deposito
+        fields = '__all__'
