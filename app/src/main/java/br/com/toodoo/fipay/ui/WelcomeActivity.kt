516e5ca8ef7882663f4773021fa03edd48dffb6c
valdirapn <valdiraires.pn@gmail.com>
2022-02-01 13:49:19 -0300

    feat: SignUp Activity

--- app/src/main/java/br/com/toodoo/fipay/ui/WelcomeActivity.kt
+++ app/src/main/java/br/com/toodoo/fipay/ui/WelcomeActivity.kt
@@ -1,9 +1,12 @@
 package br.com.toodoo.fipay.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.view.WindowCompat
 import br.com.toodoo.fipay.R
+import br.com.toodoo.fipay.ui.authentication.SignUpActivity
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +14,9 @@ class WelcomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_welcome)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        findViewById<Button>(R.id.btnGoToSignUp).setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
     }
 }
